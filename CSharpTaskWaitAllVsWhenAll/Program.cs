d9e71c0208e7adc2ab1f648846ad76b84bda76a1
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-02-15 02:11:50 +0530

    initial commit

---
+++ CSharpTaskWaitAllVsWhenAll/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpTaskWaitAllVsWhenAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Task.Delay(3000).GetAwaiter().GetResult();
+            Console.WriteLine("some other work...");
+        }
+
+        static async Task Test()
+        {
+            var t1 = Task.Run(async () => {
+                await Task.Delay(2000);
+                Console.WriteLine("task 1"); 
+            });
+            var t2 = Task.Run(() => Console.WriteLine("task 2"));
+            var t3 = Task.Run(() => Console.WriteLine("task 3"));
+
+            Task.WaitAll(new Task[] { t1, t2, t3 });
+
+            
+        }
+    }
+}
